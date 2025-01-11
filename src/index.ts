d02672db9ddf90b98d46b955965181bbeecd32d9
Vinodh <vinodh.jeevanantham@gmail.com>
2025-01-11 10:44:33 +0530

    auth verify added in context

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { CfJwtAuthDataSource } from './datasources';
 import { YogaSchemaDefinition, createYoga } from 'graphql-yoga';
 import { drizzle } from 'drizzle-orm/d1';
 import { schema } from './schemas';
+import { verifyToken } from './resolvers/mutations/helper/jwtUtils';
 
 export interface Env {
   DB: D1Database;
@@ -14,24 +15,35 @@ export interface YogaInitialContext {
     cfJwtAuthDataSource: CfJwtAuthDataSource;
   };
   jwtSecret: string;
+  accessToken: string | null;
 }
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     const url = new URL(request.url);
     const db = drizzle(env.DB);
+    //const authorization = event.headers.Authorization || event.headers.authorization;
     if (url.pathname === '/graphql') {
       const yoga = createYoga({
         schema: schema as YogaSchemaDefinition<object, YogaInitialContext>,
         landingPage: false,
         graphqlEndpoint: '/graphql',
-        context: () => ({
-          
-          datasources: {
-            cfJwtAuthDataSource: new CfJwtAuthDataSource({ db }),
-          },
-          jwtSecret: env.JWT_SECRET,
-        }),
+        context: () => {
+          const authorization = request.headers.get('Authorization') || request.headers.get('authorization');
+          let accessToken: string | null = null;
+          if (authorization) {
+            // check for auth
+            accessToken = authorization.replace(/bearer /i, '').replace(/Bearer /i, '');
+            verifyToken(accessToken, env.JWT_SECRET);
+          }
+          return {
+            datasources: {
+              cfJwtAuthDataSource: new CfJwtAuthDataSource({ db }),
+            },
+            jwtSecret: env.JWT_SECRET,
+            accessToken,
+          };
+        },
       });
       return yoga.fetch(request);
     }
