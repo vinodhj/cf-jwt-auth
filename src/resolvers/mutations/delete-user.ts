722a11badce3922bac8b56f658a000754c74b1ec
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-24 13:24:01 +0530

    casl implemented for delete user, WIP

--- src/resolvers/mutations/delete-user.ts
+++ src/resolvers/mutations/delete-user.ts
@@ -3,14 +3,32 @@ import { DeleteUserInput } from 'generated';
 import { GraphQLError } from 'graphql';
 import { Role } from 'db/schema/user';
 import { validateUserAccess } from './helper/userAccessValidators';
+import { defineAbilitiesFor } from '@src/handlers/abilities';
+import { subject } from '@casl/ability';
 
 export const deleteUser = async (
   _: unknown,
   { input }: { input: DeleteUserInput },
-  { datasources, accessToken, role }: { datasources: { cfJwtAuthDataSource: CfJwtAuthDataSource }; accessToken: string | null; role: Role }
+  {
+    datasources,
+    accessToken,
+    role,
+    user,
+    ability,
+  }: {
+    datasources: { cfJwtAuthDataSource: CfJwtAuthDataSource };
+    accessToken: string | null;
+    role: Role;
+    user: { id: string; role: Role; email: string };
+    ability: ReturnType<typeof defineAbilitiesFor>;
+  }
 ) => {
   try {
-    validateUserAccess(accessToken, role);
+    if (!ability.can('delete', subject('User', { id: input.id }))) {
+      throw new GraphQLError('Access denied: You do not have permission to delete this user', {
+        extensions: { code: 'UNAUTHORIZED' },
+      });
+    }
     // Delete user
     return await datasources.cfJwtAuthDataSource.deleteUser(input);
   } catch (error) {
